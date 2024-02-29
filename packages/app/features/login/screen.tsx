4c0d801ecf8422f8e6c57b0805e3e06934008ba7
Philippe Plaïa <philippeplaia@gmail.com>
2024-02-29 11:39:21 +0100

    yarn modif

--- packages/app/features/login/screen.tsx
+++ packages/app/features/login/screen.tsx
@@ -28,8 +28,8 @@ export function LoginScreen() {
           </XStack>
           <YStack f={1} jc="center" ai="center" >
           
-          <Input size="$3" mb={20} bw={2} bbc="grey" w={250}/>
-          <Input size="$3" mb={100} bw={2} bbc="grey" w={250}/>
+          <Input size="$3" mb={20} bw={2} bbc="grey" w={250}  defaultValue="User Name" />
+          <Input size="$3" mb={100} bw={2} bbc="grey" w={250}  defaultValue="Password" />
 
           <XStack>
             <Button {...linkProps} size="$4" bg="#6750a4" col="white" w={250}>Login</Button>
--- packages/app/features/spacecraft/detail-screen.tsx
+++ packages/app/features/spacecraft/detail-screen.tsx
@@ -1,6 +1,7 @@
-import { Button, Paragraph, YStack, ScrollView, ListItem, Card } from '@my/ui'
+import { Button, Paragraph, YStack, ScrollView, ListItem, Card, Spinner } from '@my/ui'
 import React from 'react'
 import { useQuery } from "@tanstack/react-query";
+import { Airplay, BadgeEuro } from '@tamagui/lucide-icons';
 
 
 async function fetchData() {
@@ -18,7 +19,11 @@ export function StarshipFeedScreen() {
 
   if (isLoading) {
     console.log("loading");
-    return <Paragraph>Loading...</Paragraph>;
+    return (
+    <YStack s={4} padding={4}>
+    <Spinner size="large" color="$purple10" />
+    </YStack>
+    )
   }
   
   if (isError) {
@@ -30,13 +35,16 @@ export function StarshipFeedScreen() {
     <ScrollView>
       <YStack s={4} padding={4}>
         {data.map((starship, index) => (
-          <Card key={index} br="md" borderWidth={1} padding={4}>
+          <Card key={index} br="md" borderWidth={1} padding={14} margin={20}>
             <Paragraph fw="bold" fontSize="lg">Name: {starship.name}</Paragraph>
             <Paragraph>Model: {starship.model}</Paragraph>
             <Paragraph>Manufacturer: {starship.manufacturer}</Paragraph>
             <Paragraph>Hyperdrive Rating: {starship.hyperdrive_rating}</Paragraph>
             <Paragraph>Cost in Credits: {starship.cost_in_credits}</Paragraph>
             {/* Render other properties as needed */}
+            <Button bc="#6750a4" color="white" icon={<BadgeEuro size="$3" />}>
+            Buy it now !
+      </Button>
           </Card>
         ))}
       </YStack>
